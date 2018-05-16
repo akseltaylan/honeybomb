3feeae5a4ce962dbf5abb76265278433dc466e2d
akseltaylan <akseltaylan@tamu.edu>
2018-05-16 01:40:11 -0500

    UI / honey system
    refined honey gathering system and added some UI

---
+++ HoneyBomb/Assets/Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    public Image currentHoneyBar;
+    public Image warningIcon;
+
+    public static UIManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.Log("Warning: multiple " + this + " in scene!");
+        }
+    }
+}
